65d8cab34d86f5891bc133b9322dc88f3636007f
myucesan <my96@live.nl>
2025-05-02 06:53:31 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 #![allow(unused_imports)]
+
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    println!("Broker has commenced.");
 
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let message_size: i32 = 4;
+                let correlation_id: i32 = 7;
+                // TODO: Refactor message handling for scalability later.
+                let mut response = Vec::new();
+                response.extend(&message_size.to_be_bytes());
+                response.extend(&correlation_id.to_be_bytes());
+
+                stream.write_all(&response).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
