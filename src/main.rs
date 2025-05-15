f2113f307202ce52054aec61b9e3f082c1e278a9
myucesan <my96@live.nl>
2025-05-15 07:30:32 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,35 @@ use std::net::TcpListener;
 
 fn main() {
     println!("Broker has commenced.");
+    // TODO: Refactor so its fixed size later
+    // let mut request_buffer: Vec<u8> = Vec::new();
+    let mut request_buffer = vec![0u8; 1024];
 
+    let mut response_buffer: Vec<u8> = Vec::new();
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
                 let message_size: i32 = 4;
-                let correlation_id: i32 = 7;
-                // TODO: Refactor message handling for scalability later.
-                let mut response = Vec::new();
-                response.extend(&message_size.to_be_bytes());
-                response.extend(&correlation_id.to_be_bytes());
+                let mut correlation_id: u32 = 7;
+                while (stream.read(&mut request_buffer).unwrap() > 0) {
+                    println!("First bytes are read");
+                    if let Ok(bytes) = request_buffer[8..12].try_into() {
+                        correlation_id = u32::from_be_bytes(bytes);
+                        println!("correlation id is {}", correlation_id);
+                    }
+                    // TODO: Refactor message handling for scalability later.
+                    response_buffer.extend(&message_size.to_be_bytes());
+                    response_buffer.extend(&correlation_id.to_be_bytes());
 
-                stream.write_all(&response).unwrap();
+                    stream.write_all(&response_buffer).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    
 }
